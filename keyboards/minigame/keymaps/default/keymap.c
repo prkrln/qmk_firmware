b28f972c6010abb979025f36a2dc82bdc6737760
Hippo5412 <parkerlane003@gmail.com>
2025-10-28 19:23:04 -0500

    fixed keymap and edited key layout

--- keyboards/minigame/keymaps/default/keymap.c
+++ keyboards/minigame/keymaps/default/keymap.c
@@ -8,9 +8,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ┌───┬───┬───┬───┬───┬───┐
      * │ESC│ 1 │ 2 │ 3 │ 4 │ 5 │
      * ├───┼───┼───┼───┼───┼───┤
-     * │GRV│ Q │ W │ E │ R │ T │
+     * │TAB│ Q │ W │ E │ R │ T │
      * ├───┼───┼───┼───┼───┼───┤
-     * │TAB│ A │ S │ D │ F │ G │
+     * │CTL│ A │ S │ D │ F │ G │
      * ├───┼───┼───┼───┼───┼───┤
      * │SFT│ Z │ X │ C │ V │ B │
      * ├───┼───┼───┼───┼───────┤
@@ -19,15 +19,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [0] = LAYOUT_minigame(
         KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,
-        KC_GRV, KC_Q, KC_W, KC_E, KC_R, KC_T,
-        KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G,
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,
+        KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,
         KC_LCTL, MO(1), KC_LGUI, KC_LALT, KC_SPC
         ),
     [1] = LAYOUT_minigame(
         KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,
-        KC_GRV, KC_Q, KC_W, KC_E, KC_R, KC_T,
-        KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G,
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,
+        KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,
         KC_LCTL, KC_TRNS, KC_LGUI, KC_LALT, KC_SPC
         )
